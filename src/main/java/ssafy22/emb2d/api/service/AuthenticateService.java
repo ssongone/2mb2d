2a072940e28b63f5f169253cf392647735112c70
ssongone <41146676+ssongone@users.noreply.github.com>
2024-02-05 21:35:40 +0900

    Fix: 비밀번호 숨김처리

--- src/main/java/ssafy22/emb2d/api/service/AuthenticateService.java
+++ src/main/java/ssafy22/emb2d/api/service/AuthenticateService.java
@@ -1,14 +1,20 @@
 package ssafy22.emb2d.api.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticateService {
+  @Value("${2mb2d.password}")
+  private String applicationPassword;
+
   public Boolean checkPassword(String password) {
     Boolean result = false;
-    if (password.equals("qkrtjdeo"))
+    if (password.equals(applicationPassword))
       result = true;
-    
+
     return result;
   }
+
+
 }
