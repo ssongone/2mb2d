b2c8e21a95af08e6bf846b32a699f916ab0301fb
ssongone <41146676+ssongone@users.noreply.github.com>
2024-02-05 21:05:10 +0900

    Chore: 서버 업데이트에 따른 수동 데이터 백업

--- src/main/java/ssafy22/emb2d/Application.java
+++ src/main/java/ssafy22/emb2d/Application.java
@@ -7,12 +7,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 @SpringBootApplication
 public class Application {
 
 	final public static List<String> MEMBERS = Arrays.asList(
 			"김송원", "김인겸", "김준홍", "문세진", "박승헌", "변재혁", "성열민", "송민석", "송제강", "여종원",
-			"오정현", "이유단", "이지예", "이훈재", "장건수", "장윤석", "전다윤", "전보름", "전혜림", "정윤호",
+			"오정현", "이유단", "이지예", "이훈재", "장건수", "장윤석", "전다윤", "전보름", "전혜림",
 			"주호성", "진홍엽", "최용현", "최유정", "하성표", "한영찬", "홍예원");
 
 	public static List<String> SEAT_FIXED = new ArrayList<>(List.of(
@@ -24,6 +25,7 @@ public class Application {
 	final public static int MEMBERS_COUNT = MEMBERS.size();
 
 	public static void main(String[] args) {
+		TeamAllocator.init();
 		SpringApplication.run(Application.class, args);
 	}
 
